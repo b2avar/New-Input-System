ee70e9616f873bb2b751ad377da79590c4b53f5b
Burak Avar <burakavar@gmail.com>
2023-09-13 21:52:53 +0300

    Actions unity events control

--- Assets/Scripts/TestingInputSystem.cs
+++ Assets/Scripts/TestingInputSystem.cs
@@ -2,19 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TestingInputSystem : MonoBehaviour
 {
     private Rigidbody sphereRigidbody;
+    private PlayerInput playerInput;
 
     private void Awake()
     {
         sphereRigidbody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    public void Jump()
+    public void Jump(InputAction.CallbackContext context)
     {
-        Debug.Log("Jump");
-        sphereRigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        Debug.Log(context);
+        if (context.performed)
+        {
+            Debug.Log("Jump" + context.phase);
+            sphereRigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        }
     }
 }
